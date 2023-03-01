43bc689e097f5fdac1b7cf1eb1f609b427f3f842
Wiecek-K <krystiandwiecek@gmail.com>
2023-03-01 15:18:28 +0100

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -4,18 +4,33 @@ const form = document.querySelector(".form");
 
 form.addEventListener("submit", (event) => {
 	event.preventDefault();
+
 	const firstDelay = form.querySelector('input[name="delay"]').value;
 	const stepDelay = form.querySelector('input[name="step"]').value;
 	const amount = form.querySelector('input[name="amount"]').value;
 	console.log(stepDelay, amount);
+	let position = 1;
+	createPromise(firstDelay, position)
+		.then((user) => console.log(user))
+		.catch((error) => console.error(error))
+		.finally(() => {
+			position++;
+			createPromise(stepDelay, position);
 
+			position++;
+			if (position < amount) {
+				// createPromise(stepDelay, position);
+			} else {
+				return;
+			}
+		});
 	form.reset();
 });
 
 function createPromise(position, delay) {
-	const shouldResolve = Math.random() > 0.3;
-	const promise = new Promise((resolve, reject) => {
+	return new Promise((resolve, reject) => {
 		setTimeout(() => {
+			const shouldResolve = Math.random() > 0.3;
 			if (shouldResolve) {
 				resolve({ position, delay });
 			} else {
@@ -23,11 +38,21 @@ function createPromise(position, delay) {
 			}
 		}, delay);
 	});
-	promise.then(({ position, delay }) => {
-		console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-	});
-	promise.catch(({ position, delay }) => {
-		console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-	});
+
+	// const promise = new Promise((resolve, reject) => {
+	// 	setTimeout(() => {
+	// 		if (shouldResolve) {
+	// 			resolve({ position, delay });
+	// 		} else {
+	// 			reject({ position, delay });
+	// 		}
+	// 	}, delay);
+	// });
+	// promise.then(({ position, delay }) => {
+	// 	console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+	// });
+	// promise.catch(({ position, delay }) => {
+	// 	console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+	// });
+	// promise.finally(() => {});
 }
-createPromise(2, 100);
