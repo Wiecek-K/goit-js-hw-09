5443ce2606c63249fb8bd1a612f72ed2b21f80cc
Wiecek-K <113645679+Wiecek-K@users.noreply.github.com>
2023-03-02 13:39:09 +0000

    Deploying to gh-pages from @ Wiecek-K/goit-js-hw-09@d5d6c77057a24744474d809e5309d083cc74d590 🚀

---
+++ 01-color-switcher.cb66ed58.js
@@ -0,0 +1,2 @@
+!function(){const e=document.querySelector("body"),t=document.querySelector("button[data-start]"),d=document.querySelector("button[data-stop]");function o(){return`#${Math.floor(16777215*Math.random()).toString(16)}`}d.disabled=!0,t.addEventListener("click",(()=>{setInterval((()=>e.style.backgroundColor=o()),1e3);t.disabled=!0,d.disabled=!1})),d.addEventListener("click",(()=>{clearInterval(changeBodyColorID),d.disabled=!0,t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.cb66ed58.js.map
